d6507a642c91423923129b16c6148c0ebc254f04
Juanpa Marin <juanpamarinaguilar01@gmail.com>
2023-03-25 23:38:47 -0600

    Header bien hecho

--- src/app/components/interface/footer/footer.component.ts
+++ src/app/components/interface/footer/footer.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  hotelName = 'Hotel 5 Rosas';
+
 }
--- src/app/components/interface/header-home/header-home.component.ts
+++ src/app/components/interface/header-home/header-home.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class HeaderHomeComponent {
   hotelName = 'Hotel 5 Rosas';
 
-
+  username = 'Admin'
 }
