77dde568591e62df778f21580b32bdfab3b48d92
Juanpa Marin <juanpamarinaguilar01@gmail.com>
2023-04-07 13:27:22 -0600

    Changes Modify Home Page TextArea & Div with scroll

--- src/app/components/Pages/modify-by-pages/modify-home-page/modify-home-page.component.ts
+++ src/app/components/Pages/modify-by-pages/modify-home-page/modify-home-page.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
 import { HomeServiceService } from 'src/app/components/services/home-service/home-service.service';
-
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-modify-home-page',
   templateUrl: './modify-home-page.component.html',
-  styleUrls: ['./modify-home-page.component.css']
+  styleUrls: ['./modify-home-page.component.css'],
 })
 export class ModifyHomePageComponent {
-
   information: string = '';
-  imageHotel: string = ''
+  imageHotel: string = '';
 
-  constructor(private service: HomeServiceService) {
-    this.service.getHomeInfo().subscribe(hotelInfo =>{
-      this.imageHotel = hotelInfo.imagen
-      this.information = hotelInfo.informacion
-      console.log(hotelInfo)
-    })
+  constructor(
+    private service: HomeServiceService,
+    private clipboard: Clipboard
+  ) {
+    this.service.getHomeInfo().subscribe((hotelInfo) => {
+      this.imageHotel = hotelInfo.imagen;
+      this.information = hotelInfo.informacion.replace(/\s/g,' ');
+      console.log(hotelInfo);
+    });
   }
 
 }
