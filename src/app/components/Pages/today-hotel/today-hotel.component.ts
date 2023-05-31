253f117f57653468ba651d3b0832e0137044edca
MarioQuirosL <marioql.dev@gmail.com>
2023-05-31 09:09:15 -0600

    Download report of hotel today

--- src/app/components/Pages/today-hotel/today-hotel.component.ts
+++ src/app/components/Pages/today-hotel/today-hotel.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HomeServiceService } from '../../services/home-service/home-service.service';
 
+import * as jspdf from 'jspdf';
+import 'jspdf-autotable';
+import { UserOptions } from 'jspdf-autotable';
+
+interface jsPDFWithPlugin extends jspdf.jsPDF {
+  autoTable: (options: UserOptions) => jspdf.jsPDF;
+}
+
 @Component({
   selector: 'app-today-hotel',
   templateUrl: './today-hotel.component.html',
@@ -13,8 +21,8 @@ export class TodayHotelComponent {
 
   username: String = "";
 
-  constructor(private route: ActivatedRoute,private service: HomeServiceService) {
-    this.service.getHotelToday().subscribe(rooms =>{
+  constructor(private route: ActivatedRoute, private service: HomeServiceService) {
+    this.service.getHotelToday().subscribe(rooms => {
       this.rooms = rooms;
     });
   }
@@ -27,6 +35,20 @@ export class TodayHotelComponent {
 
   myDate = new Date();
 
+  exportToPDF() {
+    const doc = new jspdf.jsPDF('portrait', 'px', 'a4') as jsPDFWithPlugin;
+
+    doc.autoTable({
+      didDrawPage: function (data: any) {
+        doc.setFontSize(14);
+        doc.setTextColor(40);
+        doc.text('Estado de habitaciones del dia de hoy del Hotel 5 Rosas', data.settings.margin.left, 20);
+      },
+      head: [['Room Number', 'Room Type', 'Room Status']],
+      body: this.rooms.map((row: { numero_Habitacion: any; nombre: any; estado_Del_Dia: any; }) => [row.numero_Habitacion, row.nombre, row.estado_Del_Dia]),
+    });
+    doc.save('HotelToday.pdf');
+  }
 
 
 }
